f8046c0f2cab9a22539d9fa9756e71db51f96a21
maxlee <maxplee8@gmail.com>
2023-11-14 23:16:47 -0500

    onboarding pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
+import {auth} from '../components/Firebase';
+import Onboard from '../pages/Onboard.js';
 
 import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack} from "@chakra-ui/react"
 
 function Home() {
     return (
         <Box bg={'red'} fontSize={'40px'}>
-            <Center>
-                <Text>text</Text>
+            {auth?.currentUser ? (
+                <Center>
+                    <Heading>Home</Heading>
+                </Center>
+            ) : (
+                <Onboard/>
 
-            </Center>
+            )}
 
 
         </Box>
---
+++ src/pages/Onboard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import OnboardLeader from "./OnboardLeader";
+
+
+import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack} from "@chakra-ui/react"
+
+function Onboard() {
+    return (
+        <Box bg={'red'} fontSize={'40px'}>
+            <Center>
+                <Heading>Onboard</Heading>
+            </Center>
+
+
+        </Box>
+    );
+}
+
+export default Onboard;
