967bb40130637182af300dc78e763a167fbd2705
singularity <maxpeng48@gmail.com>
2023-11-14 23:53:48 -0500

    Upload images

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,14 +5,22 @@ import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack} f
 
 export default function Screen1() {
     return (
-        <Box bg={'red'} fontSize={'40px'}>
-            <Center>
-                <Text>text</Text>
-
-            </Center>
-
-
+        <Center>
+        <Box bg={''} fontSize={'40px'} m={'2em'} borderRadius={'lg'} borderWidth={'4px'} h={'30em'} w={'20em'} overflow={'hidden'}>
+                <Box alignItems={'baseline'} justifyContent={"center"} ml={'2em'}> 
+                    <Text>text</Text>
+                </Box>
+                <Box ml={'2em'}>
+                    <Box className='imageWrapper'>
+                        <Image 
+                            boxSize='4em'
+                            src='img\omega.png'
+                            alt='test org image'
+                        />
+                    </Box>
+                </Box>
         </Box>
+        </Center>
     );
 
 }
