05596bfc37d830fb516c76b8392c927a6f2cc7a0
maxlee <maxplee8@gmail.com>
2023-11-14 22:22:17 -0500

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
+            <Route path="/abouta" element={<About />} />
         </Routes>
     </Router>
 
