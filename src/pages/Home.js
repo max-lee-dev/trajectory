b49aa3e9419d8bf0d3a2c7a72d968c1c82bda98f
maxlee <maxplee8@gmail.com>
2023-11-14 22:06:40 -0500

    pages

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Home() {
+    return (
+        <div class="">
+            <h1 class={"text-3xl"}>Home</h1>
+            <p>This is the home page.</p>
+        </div>
+    );
+
+}
+
+export default Home;
