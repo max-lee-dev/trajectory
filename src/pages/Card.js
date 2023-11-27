3768c5adbc5c0a6460777b2370a8353feb661c85
singularity <maxpeng48@gmail.com>
2023-11-26 23:17:42 -0500

    card work

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 
-import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack} from "@chakra-ui/react"
+import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack, SimpleGrid} from "@chakra-ui/react"
 
-export default function Card({img, name}){
+export default function Card({orgObj}){
     return (
-        <Center>
-        <Box bg={''} fontSize={'40px'} m={'2em'} borderRadius={'lg'} borderWidth={'4px'} h={'30em'} w={'20em'} overflow={'hidden'}>
-            <Box alignItems={'baseline'} justifyContent={"center"} ml={'2em'}> 
-                <Text>{name}</Text>
+        <Box bg={''} fontSize={'40px'}  borderRadius={'lg'} borderWidth={'1px'} h={'6em'}  overflow={'hidden'}>
+            <Box ml={'12px'}> 
+                <Text fontSize={'md'} as={'bold'} >{orgObj.name}</Text>
             </Box>
             <Center>
                 <Box ml={'2em'}>
                     <Box className='imageWrapper'>
-                        <Image boxSize={'md'}
+                        <Image boxSize={'2em'}
+                            // src={orgObj.img}
                             src='img\omega.png'
                             alt='test org image'
                         />
@@ -21,7 +21,6 @@ export default function Card({img, name}){
                 </Box>
             </Center>
         </Box>
-        </Center>
     );
 
 }
