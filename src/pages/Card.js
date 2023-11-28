21cd408e9b6a073224198791206433148f41a652
singularity <maxpeng48@gmail.com>
2023-11-28 01:15:52 -0500

    trying to use react
    wa

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -5,21 +5,19 @@ import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack, S
 
 export default function Card({orgObj}){
     return (
-        <Box bg={''} fontSize={'40px'}  borderRadius={'lg'} borderWidth={'1px'} h={'6em'}  overflow={'hidden'}>
-            <Box ml={'12px'}> 
-                <Text fontSize={'md'} as={'bold'} >{orgObj.name}</Text>
+        <Box bg={''} fontSize={'40px'}  borderRadius={'lg'} borderWidth={'1px'} h={'8em'}  overflow={'hidden'}>
+            <Box className='imageWrapper'>
+                <Image boxSize={'5em'}
+                    // src={orgObj.img}
+                    src='img\omega.png'
+                    alt='test org image'
+                />
             </Box>
-            <Center>
-                <Box ml={'2em'}>
-                    <Box className='imageWrapper'>
-                        <Image boxSize={'2em'}
-                            // src={orgObj.img}
-                            src='img\omega.png'
-                            alt='test org image'
-                        />
-                    </Box>
-                </Box>
-            </Center>
+            <Box ml={'6px'}> 
+                <Box><Text fontSize={'md'} as={'b'} >{orgObj.name}</Text></Box>
+                <Box><Text fontSize={'md'}>desc</Text></Box>
+                <Box><Text fontSize={'md'} >sub 2</Text></Box>
+            </Box>
         </Box>
     );
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,41 @@ import React, {useEffect, useState} from 'react';
 import {auth} from '../components/Firebase';
 import Onboard from '../pages/Onboard.js';
 import Card from './Card.js';
-import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack} from "@chakra-ui/react";
+import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack, SimpleGrid} from "@chakra-ui/react";
 import NewAccountForm from "./NewAccountForm.js";
 
 function Home() {
 
-    const [user, setUser] = useState(null);
+    const [user, setUser, ] = useState(null);
+    const [showGrid] = useState(false);
     useEffect(() => {
         auth.onAuthStateChanged(userAuth => {
             setUser(userAuth);
         })
     }, [auth])
+
+       const orgs = [
+        {
+            name: 'Omega Robotics',
+            img: 'img\omega.png',
+        },
+        {
+            name: 'cool org 2',
+            img: 'img\omega.png',
+        }  ,
+        {
+            name: 'cool org 3',
+            img: 'img\omega.png',
+        }  ,
+        {
+            name: 'cool org 4',
+            img: 'img\omega.png',
+        }  ,
+        {
+            name: 'cool org 5',
+            img: 'img\omega.png',
+        }  
+    ]
     return (
         <Box bg={'transparent'} fontSize={'40px'}>
             {auth.currentUser?.displayName && (
@@ -26,13 +50,23 @@ function Home() {
 
             {auth?.currentUser && (
                 <Box paddingTop={'100px'} display={'flex'} justifyContent={'space-evenly'}>
-
                     <NewAccountForm/>
                 </Box>
             )}
 
             {!auth?.currentUser && (
                 <Box>
+                    <Center>
+                        {/* <Button onClick={() => console.log(showGrid)}>
+                            testing home page
+                        </Button> */}
+                            <Box>
+                                <SimpleGrid  columns={5} spacing={'2em'}>
+                                    {orgs.map(org=> <Card key={org.name} orgObj={org}/> )} 
+                                </SimpleGrid>
+                            </Box>
+                            
+                   </Center>
                     <Onboard/>
 
                 </Box>
