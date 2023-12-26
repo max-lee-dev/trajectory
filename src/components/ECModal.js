4eac49d64bcbb1451bd28e82698334d8b980934b
maxlee <maxplee8@gmail.com>
2023-12-26 01:44:59 -0500

    modal

---
+++ src/components/ECModal.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    Text,
+    Box,
+    VStack,
+    HStack,
+    Center,
+    useDisclosure
+} from "@chakra-ui/react"
+
+function ECModal({orgObj, isOpen, onClose}) {
+    return (
+        <Modal isCentered size={["xs", "md", "md"]} isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay/>
+            <ModalContent>
+                <ModalHeader fontFamily={"Poppins"}>
+
+                    <Text fontSize={'28px'} w={'95%'}>
+                        {orgObj.name}
+                    </Text>
+                    <Text fontSize={'16px'} color={'gray'}>
+                        {orgObj.major}
+                    </Text>
+                    <HStack paddingTop={3} fontFamily={"Poppins"} fontWeight={400} fontSize={'10px'} color='white'
+                            display={'flex'}
+                            justifyContent={'space-between'}>
+                        <Box display={'flex'} justifyContent={'center'} bg={orgObj.size === "3" ? "black" : "#D9D9D9"}
+                             borderRadius={4}
+                             h={'30px'}
+                             color={orgObj.size === "3" ? "white" : "black"}
+                             width={'100%'}>
+                            <Center>
+                                <Text>  {orgObj.size === '1' ? 'Low ' : orgObj.size === '2' ? 'Medium ' : 'Large '} Size</Text>
+                            </Center>
+                        </Box>
+                        <Box display={'flex'} justifyContent={'center'} bg={orgObj.impact === "3" ? "black" : "#D9D9D9"}
+                             borderRadius={4}
+                             h={'30px'}
+                             color={orgObj.impact === "3" ? "white" : "black"}
+                             width={'100%'}>
+                            <Center>
+                                <Text>
+                                    {orgObj.impact === '1' ? 'Low ' : orgObj.impact === '2' ? 'Medium ' : 'High '} Impact
+                                </Text>
+
+                            </Center>
+                        </Box>
+                        <Box display={'flex'} justifyContent={'center'}
+                             color={orgObj.momentum === "3" ? "white" : "black"}
+                             bg={orgObj.momentum === "3" ? "black" : "#D9D9D9"} borderRadius={4}
+                             h={'30px'}
+                             width={'100%'}>
+                            <Center>
+                                <Text>{orgObj.momentum === "1" ? 'Low ' : orgObj.momentum === "2" ? 'Medium ' : 'High '}Growth</Text>
+                            </Center>
+                        </Box>
+                    </HStack>
+                </ModalHeader>
+                <ModalCloseButton/>
+                <ModalBody>
+
+
+                    <Text overflowY={'auto'} h={'200px'} fontSize={'16px'}>{orgObj.description}</Text>
+
+                    <Center fontFamily={"Poppins"} padding={5}>
+                        <Box display={'flex'} justifyContent={'center'} borderRadius='5px' minWidth="100%" h='40px'
+                             bg={'black'} color={'white'}
+                             mr={3} as='a'
+                             href={orgObj.website}
+                             fontSize={'16px'}>
+
+                            <Center fontWeight={500} textAlign={'center'}>
+                                Website
+                            </Center>
+                        </Box>
+                    </Center>
+                </ModalBody>
+
+            </ModalContent>
+        </Modal>
+    )
+}
+
+export default ECModal
--- src/components/findECs/findECsCard.js
+++ src/components/findECs/findECsCard.js
@@ -1,20 +1,57 @@
 import React from 'react';
+import ECModal from '../ECModal.js';
 
-import {Container, Grid, Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, SimpleGrid, Stack, HStack, VStack, Badge, LinkBox, LinkOverlay,Menu,MenuButton,MenuList,MenuItem,MenuItemOption,MenuGroup,MenuOptionGroup,MenuDivider,IconButton, Divider} from "@chakra-ui/react"
+import {
+    Container,
+    Grid,
+    Box,
+    Button,
+    Center,
+    Flex,
+    Heading,
+    Image,
+    Link,
+    Spacer,
+    Text,
+    SimpleGrid,
+    Stack,
+    HStack,
+    VStack,
+    Badge,
+    LinkBox,
+    LinkOverlay,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuDivider,
+    IconButton,
+    Divide,
+    useDisclosure
+} from "@chakra-ui/react"
 import {TriangleUpIcon, HamburgerIcon} from '@chakra-ui/icons'
 
-export function Card({orgObj}){
+export function Card({orgObj}) {
+
+    const {isOpen, onOpen, onClose} = useDisclosure()
     if (!/^https?:\/\//i.test(orgObj.website)) {
         orgObj.website = 'https://' + orgObj.website;
-    } 
-    let screenWidth = 100.0/9*(parseInt(orgObj.size) + parseInt(orgObj.impact) + parseInt(orgObj.momentum)) + "%";
-     
+    }
+    let screenWidth = 100.0 / 9 * (parseInt(orgObj.size) + parseInt(orgObj.impact) + parseInt(orgObj.momentum)) + "%";
+
 
     return (
-        <LinkBox 
-            bg={'white'}  color={'black'} borderRadius={'lg'} borderWidth={'1px'}  boxShadow={'md'} overflow={'hidden'} maxW={'10em'}  p={'0px'}
-            _hover={{bg:'lightgray'}}
-        >           
+
+        <LinkBox
+            bg={'white'} color={'black'} borderRadius={'lg'} borderWidth={'1px'} boxShadow={'md'} overflow={'hidden'}
+            maxW={'10em'} p={'0px'}
+            _hover={{bg: 'lightgray'}}
+            onClick={onOpen}
+        >
+            <ECModal orgObj={orgObj} isOpen={isOpen} onClose={onClose}/>
 
             {/* <Menu>
                 <MenuButton position={'absolute'}
@@ -29,31 +66,33 @@ export function Card({orgObj}){
                 </MenuList>
             </Menu> */}
 
-            <Flex m={'6px'} direction={'column'}> 
-                <Flex direction = {"row"}>
+
+            <Flex m={'6px'} direction={'column'}>
+                <Flex direction={"row"}>
                     <Text fontSize={'md'} fontWeight={'bold'} lineHeight={'160%'} as={'h5'}>
-                        <LinkOverlay href={orgObj.website}>
-                        <Badge ml={"6px"} mr={'3px'} as={'span'} bg={'#f5f4f0ff'}> {orgObj.major} </Badge>  {/* badges will be added from db */}
-                            {orgObj.name} {/* THIS IS THE NAME OF THE ORG */ }
-                        </LinkOverlay>
+                        <Badge ml={"6px"} mr={'3px'} as={'span'}
+                               bg={'#f5f4f0ff'}> {orgObj.major} </Badge> {/* badges will be added from db */}
+                        {orgObj.name} {/* THIS IS THE NAME OF THE ORG */}
                     </Text>
                 </Flex>
                 <Spacer/>
-                <Box><Text fontSize={'md'}>{orgObj.blurb}</Text></Box>  {/* The desc of org ( 1 line) */}
+
+                <Box><Text fontSize={'md'}>{orgObj.blurb}</Text></Box> {/* The desc of org ( 1 line) */}
                 <Spacer/>
-                <Container w={screenWidth} h={'12px'} bg={'#badbffff'} m={'4px'} borderRadius={'10px'}/> 
+                <Container w={screenWidth} h={'12px'} bg={'#badbffff'} m={'4px'} borderRadius={'10px'}/>
             </Flex>
+
         </LinkBox>
     );
 }
 
 
-export function CardGrid({orgArr, columns, selectedMajor, sortBy}){
-    if (selectedMajor != ''){
+export function CardGrid({orgArr, columns, selectedMajor, sortBy}) {
+    if (selectedMajor != '') {
         orgArr = orgArr.filter(org => org.major == selectedMajor);
     }
     // console.log(orgArr);
-    switch(sortBy) {
+    switch (sortBy) {
         case 'size':
             orgArr.sort((a, b) => b.size - a.size);
             break;
@@ -64,8 +103,9 @@ export function CardGrid({orgArr, columns, selectedMajor, sortBy}){
             orgArr.sort((a, b) => b.momentum - a.momentum);
             break;
         default:
-            orgArr.sort((a, b) => (parseInt(b.size) + parseInt(b.impact) + parseInt(b.momentum)) - (parseInt(a.size) + parseInt(a.impact) + parseInt(a.momentum)))}
-    return(
+            orgArr.sort((a, b) => (parseInt(b.size) + parseInt(b.impact) + parseInt(b.momentum)) - (parseInt(a.size) + parseInt(a.impact) + parseInt(a.momentum)))
+    }
+    return (
         <Box>
             <SimpleGrid columns={columns} spacing={'1em'}>
                 {orgArr.map(orgObj => <Card key={orgObj.name} orgObj={orgObj}/>)}
