227c75221f1efd4748a418ed52f2816ac14f60cf
maxlee <maxplee8@gmail.com>
2024-01-02 18:00:04 -0500

    colors

--- src/components/findECs/filterPage.js
+++ src/components/findECs/filterPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import '@fontsource/coiny';
 import majorColors from '../findECs/majorColors.json';
+import selectyourinterests from '../../assets/img/selectyourinterests.png';
 
 
 import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack, Grid, SimpleGrid} from "@chakra-ui/react"
@@ -9,13 +10,14 @@ function FilterPage({selectedMajorArr, majorArr, onFilterClick}) {
 
     return (
         <Center>
-            <Box bg={'transparent'} width={['90vw']} pt={['10%']} fontFamily={'coiny'} color={'white'}
+            <Box bg={'transparent'} width={['90vw']} pt={['10px']} fontFamily={'coiny'} color={'white'}
                 // borderWidth={'2px'} borderColor={'black'}
                  style={{WebkitTextStroke: '2px #360503ff'}} fontWeight={'900'}
             >
-                <Center><Text fontSize='1em' top={''}> SELECT YOUR </Text></Center>
-                <Center><Text fontSize='1em' top={''} pb={['10%']}> INTERESTS </Text></Center>
+                <Center paddingBottom={6}>
 
+                    <img src={selectyourinterests} width={'250px'}/>
+                </Center>
                 <SimpleGrid columns={2} spacing={'10px'}>
                     {majorArr.map(x => <FilterButton key={x} selectedMajorArr={selectedMajorArr} name={x}/>)}
                 </SimpleGrid>
@@ -58,9 +60,10 @@ function FilterButton({selectedMajorArr, name}) {
 
     return (
         <Box size={'lg'} bg={bgColor} pb={'10px'}
+             borderRadius={5}
              onClick={toggle}>
             <Center textAlign={'center'}>
-                <Text style={{WebkitTextStroke: '0px #FFFFFF'}} fontSize='18px' fontWeight={400} top={''}
+                <Text padding={1} style={{WebkitTextStroke: '0px #FFFFFF'}} fontSize='18px' fontWeight={400} top={''}
                       color={'white'}>
                     {name}
                 </Text>
