eaf1e20f68becef685d5d17e341dbabb737d739f
maxlee <maxplee8@gmail.com>
2024-01-02 09:24:53 -0500

    majorcolors.json

--- src/components/AppWrapper.js
+++ src/components/AppWrapper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack, Divider, Select} from "@chakra-ui/react"
-import { Renderer } from './Renderer';
+import {Renderer} from './Renderer';
 import banner from '../assets/img/trajectoryBanner.png';
 
 function AppWrapper() {
@@ -9,29 +9,29 @@ function AppWrapper() {
     return (
 
         <Box width='100%' h={'100%'} bg={'transparent'} fontFamily={"Poppins"} css={{
-                    "-webkit-touch-callout": "none",
-                    "-webkit-user-select": "none",
-                    "-moz-user-select": "none",
-                    "-ms-user-select": "none"
-                }}>
+            "-webkit-touch-callout": "none",
+            "-webkit-user-select": "none",
+            "-moz-user-select": "none",
+            "-ms-user-select": "none"
+        }}>
 
             <Center>
                 <Box width={'70%'} paddingTop={'5%'}>
                     <Center>
                         <VStack>
                             {/* <Box> */}
-                                <Box textAlign={'left'} fontSize='24px' fontWeight={500} color={'#000000ff'} pr={'8px'} pl={'8px'} bg={'transparent'} mt={'10px'}>
-                                    <Text  fontFamily={"Poppins"} fontSize={'3.5em'} zIndex={'2'} > FindECs </Text>
-                                    {/* <Image src={banner} boxSize={'auto'}/> */}
-                                    {/* <Divider bg={'black'}/> */}
-                                </Box>
-                                <Renderer columns={1}/>
+                            <Box textAlign={'left'} fontSize='24px' fontWeight={500} color={'#000000ff'} pr={'8px'}
+                                 pl={'8px'} bg={'transparent'} mt={'10px'}>
+                                {/* <Image src={banner} boxSize={'auto'}/> */}
+                                {/* <Divider bg={'black'}/> */}
+                            </Box>
+                            <Renderer columns={1}/>
                             {/* </Box> */}
                         </VStack>
                     </Center>
                 </Box>
             </Center>
-            
+
 
         </Box>
 
--- src/components/findECs/filterPage.js
+++ src/components/findECs/filterPage.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import '@fontsource/coiny';
+import majorColors from '../findECs/majorColors.json';
+
 
 import {Box, Button, Center, Flex, Heading, Image, Link, Spacer, Text, VStack, Grid, SimpleGrid} from "@chakra-ui/react"
 
@@ -7,20 +9,21 @@ function FilterPage({selectedMajorArr, majorArr, onFilterClick}) {
 
     return (
         <Center>
-            <Box bg={'transparent'} width={['90vw']} pt={['10%']} fontFamily={'coiny'} color={'white'} 
+            <Box bg={'transparent'} width={['90vw']} pt={['10%']} fontFamily={'coiny'} color={'white'}
                 // borderWidth={'2px'} borderColor={'black'}
-                style={{WebkitTextStroke: '2px #360503ff'}} fontWeight={'900'}
+                 style={{WebkitTextStroke: '2px #360503ff'}} fontWeight={'900'}
             >
-                    <Center><Text fontSize='1em' top={''}> SELECT YOUR </Text></Center>
-                    <Center><Text fontSize='1em' top={''} pb={['10%']}> INTERESTS </Text></Center>
+                <Center><Text fontSize='1em' top={''}> SELECT YOUR </Text></Center>
+                <Center><Text fontSize='1em' top={''} pb={['10%']}> INTERESTS </Text></Center>
 
-                    <SimpleGrid columns={2} spacing={'10px'}>
-                        {majorArr.map(x =>  <FilterButton key={x} selectedMajorArr={selectedMajorArr} name={x} />)}
-                    </SimpleGrid>
+                <SimpleGrid columns={2} spacing={'10px'}>
+                    {majorArr.map(x => <FilterButton key={x} selectedMajorArr={selectedMajorArr} name={x}/>)}
+                </SimpleGrid>
 
                 <Center>
-                    <Button size={'lg'} bg={'#360503ff'}  m={'40px'} style={{WebkitTextStroke: '0px #FFFFFF'}} onClick={onFilterClick} >
-                        <Text fontSize='1em' fontWeight={500} top={''} color={'white'} > 
+                    <Button size={'lg'} bg={'#360503ff'} m={'40px'} style={{WebkitTextStroke: '0px #FFFFFF'}}
+                            onClick={onFilterClick}>
+                        <Text fontSize='1em' fontWeight={500} top={''} color={'white'}>
                             FIND ECS
                         </Text>
                     </Button>
@@ -30,29 +33,39 @@ function FilterPage({selectedMajorArr, majorArr, onFilterClick}) {
     )
 }
 
-function FilterButton({selectedMajorArr, name}) { 
+function FilterButton({selectedMajorArr, name}) {
 
     const [on, setOn] = useState(true)
+    const [bgColor, setBgColor] = useState('#360503ff')
+
+
     // console.log(name)
 
     function toggle(e) {
-        e.preventDefault();
         console.log("click!")
         setOn(!on);
-        if(on){
+        if (on) {
             selectedMajorArr.add(name);
+            setBgColor(majorColors[name])
         } else {
             selectedMajorArr.delete(name);
+            setBgColor('#360503ff')
         }
         console.log(selectedMajorArr);
+
     }
 
-    return(
-        <Button size={'lg'} bg={'#360503ff'} pb={'10px'} style={{WebkitTextStroke: '0px #FFFFFF'}} onClick={toggle} >
-            <Text fontSize='1em' fontWeight={500} top={''} color={'white'} > 
-                {name}
-            </Text>
-        </Button>
+
+    return (
+        <Box size={'lg'} bg={bgColor} pb={'10px'}
+             onClick={toggle}>
+            <Center textAlign={'center'}>
+                <Text style={{WebkitTextStroke: '0px #FFFFFF'}} fontSize='18px' fontWeight={400} top={''}
+                      color={'white'}>
+                    {name}
+                </Text>
+            </Center>
+        </Box>
     )
 }
 
